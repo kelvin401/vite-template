3335fb17508d8b383c2cd15475d4cbeba793da40
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2023-09-15 09:29:38 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+export function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center py-6">
+      <div className="w-full flex justify-between p-6">
+        <h1>Hello World</h1>
+        <strong>Criar conta</strong>
+      </div>
+    </div>
+  );
+}
