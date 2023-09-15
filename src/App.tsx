f0ca939a40c6b9c6fc8bcfc925119fa2e3062f0b
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2023-09-15 20:11:27 -0300

    fix: remove unsed components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 export function App() {
   return (
-    <div className="w-screen h-screen flex justify-center py-6">
-      <div className="w-full flex justify-between p-6">
+    <div className="w-screen h-screen flex justify-center p-6">
+      <div>
         <h1>Hello World</h1>
-        <strong>Criar conta</strong>
       </div>
     </div>
   );
