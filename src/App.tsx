44ed72dff7f4f82007a90730ec6ad152561e09e6
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2023-09-15 22:46:43 -0300

    fix: back to the initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ export function App() {
   return (
     <div className="w-screen h-screen flex justify-center p-6">
       <div>
-        <h1>Hello World</h1>
+        <h1>Hello World!</h1>
       </div>
     </div>
   );
